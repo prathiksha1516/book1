49fa105e1645f4a2bd738caf9112c3b108bc86c3
prathiksha1516 <71827736+prathiksha1516@users.noreply.github.com>
2021-04-27 20:54:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from './screens/WelcomeScreen'
+
+import { AppTabNavigator } from './components/AppTabNavigator'
+
+
+export default function App() {
+  return (
+   <AppContainer/>
+  );
+}
+
+const switchNavigator = createSwitchNavigator({
+  WelcomeScreen:{screen: WelcomeScreen},
+  BottomTab:{screen: AppTabNavigator}
+})
+
+const AppContainer =  createAppContainer(switchNavigator);
